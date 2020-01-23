168b58ef8d19b6d3f13e22582a2a1e3d25976d55
Bhoopesh <bhoopeshnandan.singh@bt.com>
2020-01-23 14:03:20 +0530

    initial commit

---
+++ src/main/java/com/ms/discovery/service/awsdiscoveryservice/AwsDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ms.discovery.service.awsdiscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDiscoveryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ms/discovery/service/awsdiscoveryservice/AwsDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.discovery.service.awsdiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
