51bb94619837e4e0010d2447544b2363599d3a1c
Bhoopesh <bhoopeshnandan.singh@bt.com>
2020-01-30 21:50:55 +0530

    Initial commit

--- src/main/java/com/ms/discovery/service/awsdiscoveryservice/AwsDiscoveryServiceApplication.java
+++ src/main/java/com/ms/discovery/service/awsdiscoveryservice/AwsDiscoveryServiceApplication.java
@@ -2,8 +2,11 @@ package com.ms.discovery.service.awsdiscoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootApplication
+@EnableEurekaServer
 public class AwsDiscoveryServiceApplication {
 
 	public static void main(String[] args) {
